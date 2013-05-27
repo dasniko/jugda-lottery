c562a3022c39947434b65abe1a3e2221ad52cbb9
Niko <niko@n-k.de>
2013-05-27 17:05:45 +0200

    added shuffeling

--- jugda-lottery.js
+++ jugda-lottery.js
@@ -3,14 +3,29 @@ People = new Meteor.Collection("people");
 if (Meteor.isClient) {
     
     Template.lottery.people = function () {
-        return People.find();
+        return Session.get("people");
     };
     
     Template.lottery.events({
-        "click": function() {
-            alert("click");
+        "click .showPeople": function() {
+            Session.set("people", People.find().fetch());
+            return false;
+        },
+        "click .startLottery": function() {
+            handle = Meteor.setInterval(mixItUp, 250);
+            return false;
+        },
+        "click .stopLottery": function() {
+            Meteor.setTimeout(function() {Meteor.clearInterval(handle)}, 1000);
+            return false;
         }
     });
+    
+    mixItUp = function() {
+        var p = People.find().fetch();
+        p = _.shuffle(p);
+        Session.set("people", p);
+    };
     
 }
 
